c06fc6ff830cbdc040de91b014d70033b941bc38
teata99 <teata99@naver.com>
2023-03-22 15:14:35 +0900

    commit

--- js/eng-data.js
+++ js/eng-data.js
@@ -113,9 +113,7 @@ function countPage(params_class1, params_class2, currentPage) {
     for(let i = firstNumber; i <= lastNumber; i++) {
         appendPagination(params_class1, params_class2, i, currentPage, '');
     }
-    
-    console.log(next, pageTotal);
-    
+
     if(next <= pageTotal) {
         appendPagination(params_class1, params_class2, next, currentPage, '&raquo;');
     }
